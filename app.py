416f9b75130fda1c7b582e707f315ce2d5438289
Nithin039 <nithinsiddu03@gmail.com>
2025-04-21 12:25:40 +0530

    second commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from pythonfiles.recruiterslogin import *
 from pythonfiles.students_login import *
 from pythonfiles.Students_register import register_student
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 app.secret_key = '2345'
@@ -220,4 +221,5 @@ def api_report_data():
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
